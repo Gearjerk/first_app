5e692f3a250a5e28fdc5eb908fab3b353d891602
Gear Jerk <gearnumbernine@gmail.com>
2013-02-12 01:30:24 -0600

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '18144b08921b3347b38176308de1d623835bdb2423357ba52cb387d6fcc5d24f929d1b2a665ff62c41e66bd3f2961d8d30eb7dd602b290199d39272d11274d11'
